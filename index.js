ab6101a574a968ff0e37fa285dd4322b5d5737ef
Koima-Sam <sam17koima@gmail.com>
2022-06-07 14:47:37 +0300

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let num1=31,
+    num2=2,
+    num3 = 31,
+    num4 = 9;
 
 // Write your code here
+//returns multiplication
+let multiply = num1 * num2;
+//Generate random number
+let random = Math.floor(Math.random()*10)+1;
+//Equation that calculates remainder that is equal to four
+let mod = num3%num4;
+
+//return  as the highest number in the set
+let max = Math.max(1,2,3,4,5,6,6,17,3,20);
